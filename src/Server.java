c0023b2a91fd46fa76ec77fc1efd70c70c8d8c06
crystaleemj <ceemj_@hotmail.com>
2023-05-10 23:22:39 +0800

    first commit

---
+++ src/Server.java
@@ -0,0 +1,8 @@
+import java.net.ServerSocket;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        
+        ServerSocket serv = new ServerSocket()
+    }
+}
